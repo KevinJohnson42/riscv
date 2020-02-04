876e84219ea285d400c4f68f9fc7f4d5d5255621
Kevin Johnson <kevandjohn94@gmail.com>
2020-02-04 08:40:07 -0500

    Changed to a simple "Hello World" program.

--- program.c
+++ program.c
@@ -30,6 +30,8 @@ typedef unsigned char   u8;
 void main(); void _start(){main();}
 //DONT TOUCH
 
+
+//Some useful test functions below
 /*
 int mul(int a, int b)
 {
@@ -47,51 +49,6 @@ int factorial(int x)
 {
     if(x == 0) {return 1;}
     else {return mul(x,factorial(x-1));}
-}
-
-int mul_fast(int a, int b)
-{
-    int y = 0;
-    while((a!=0)&&(b!=0))
-    {
-        if(b&1){y += a;}
-        a = a << 1;
-        b = b >> 1;
-    }
-    return y;
-}
-
-int factorial_fast(int x)
-{
-    if(x == 0) {return 1;}
-    else {return mul_fast(x,factorial(x-1));}
-}
-
-*/
-
-void sleep(u32 time)
-{
-    u32 start, stop;
-    start = timer_value;
-    stop = start + time;
-    //Roll over wait
-    if(stop < start)
-    {
-        while(timer_value > stop){/*Wait*/}
-    }
-    //Normal wait
-    while(timer_value < stop){/*Wait*/}
-}
-
-void print(char*data)
-{
-    int i=0;
-    while(data[i])
-    {
-        while(uart_write_ready == 0) {/*WAIT*/}
-        uart_data = data[i];
-        i++;
-    }
 }
 
 int uart_rx(char*x, int size)
@@ -113,80 +70,48 @@ void print_hex(u32 a, u32 n)
         nib = (a >> (4*2*n - 4*(i+1)))&0xF;
         if(nib < 10)    {nib += 48;}
         else            {nib += 65-10;}
-        while(uart_write_ready == 0) {/*WAIT*/}
+        while(uart_write_ready == 0) {} //Wait
         uart_data = nib;
     }
 }
+*/
 
+void sleep(u32 time)
+{
+    u32 start, stop;
+    start = timer_value;
+    stop = start + time;
+    //Roll over wait
+    if(stop < start)
+    {
+        while(timer_value > stop){} //Wait
+    }
+    //Normal wait
+    while(timer_value < stop){} //Wait
+}
 
+void print(char*data)
+{
+    int i=0;
+    while(data[i])
+    {
+        while(uart_write_ready == 0) {} //Wait
+        uart_data = data[i];
+        i++;
+    }
+}
 
 void main()
 {
-
     u32 wait_cycles = 1000;
 
     while(1)
     {
-        sleep(wait_cycles);
-        print("Uart: ");
-        print_hex(uart_read_count,4);
-        print("\n");
-        io_data = ~(io_data);
+        sleep(wait_cycles);             //Read the timer
+        print("Hello World!\n");        //Write to UART, See register 14 in sim
+        io_data = ~(io_data);           //Read, invert, Write GPIO
     }
     
-    //End of program
+    //End of program safety
     while(1){}
 }
-
-
-
-
-/*
-int main()
-{
-
-    int a,b,c;
-    write(1,io_address);
-    write(2,io_address+4);
-    a = read(io_address);
-    b = read(io_address+4);
-    c = a + b;
-    write(c,io_address);
-    
-
-    
-    
-    for(int i=0;i<2;i++)
-    {
-        write_32(i+2,io_address+i*4);
-    }
-    int total = 27;
-    for(int i=0;i<2;i++)
-    {
-        total = mul(total,read_32(io_address+4*i));
-        write_32(total,io_address);
-    }
-
-    print("Hello World\n");
-    for(int i=0;i<12;i++)
-    {
-        int x = read_8(uart_address+i);
-    }
-    
-
-    
-    write_32(10,io_address);
-    int a = read_32(io_address);
-    int b = factorial(a);
-    write_32(b,io_address);
-    
-    
-    //faster
-    
-    write_32(8,io_address);
-    int a = read_32(io_address);
-    int b = factorial_fast(a);
-    write_32(b,io_address);
-    
-}
-*/
